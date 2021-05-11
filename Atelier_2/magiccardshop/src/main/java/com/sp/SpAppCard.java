ee468d39ce613975cc30463a93b5a7bbd645aafc
Enzo Contini <enzo.contini@tpi07.cpe.fr>
2021-05-11 09:26:07 +0200

    Step 0 : Init Spring project

---
+++ Atelier_2/magiccardshop/src/main/java/com/sp/SpAppCard.java
@@ -0,0 +1,12 @@
+package com.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpAppCard {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpAppCard.class,args);
+	}
+}
